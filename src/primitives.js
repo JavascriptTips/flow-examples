4899ee2fc2b72882f70b1b50ba2e070b7df30498
zhouyg <yunge@taovip.com>
2016-09-21 16:58:09 +0800

    init

---
+++ src/primitives.js
@@ -0,0 +1,27 @@
+/**
+ * 这里展示的原始基本的数据类型，
+ *
+ * number
+ *
+ * string
+ *
+ * boolean
+ *
+ * null
+ *
+ * void（undefined)
+ */
+
+// @flow
+
+var num:number = 1;
+
+var str:string = 'a';
+var str2:string = 'abc';
+
+var b:boolean = true;
+var b1:boolean = false;
+
+var n:null = null;
+
+var undef:void = undefined;
