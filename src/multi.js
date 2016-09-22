e33305bd85dc61200b3c6f3871e4705dfcde8ce9
zhouyg <yunge@taovip.com>
2016-09-22 22:22:34 +0800

    multi

--- src/multi.js
+++ src/multi.js
@@ -2,8 +2,19 @@
  * 限定于 几个值
  */
 
+//@flow
+
 /**
  * 可能有多个类型
  */
 var dd:number|string = 22
 var ee:number|string = 'abcd'
+
+
+type Ca = string | number;
+
+var c:Ca = 1;
+
+type Values = 'A' | 'B';
+
+var d:Values = 'B';
